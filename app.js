843108107578ec46ae3266b36dc2d4658a4160ab
Ahtasham Mustafa <ahtasham@polymer.com.pk>
2023-08-12 17:53:25 +0500

    Assignment 1 Done

---
+++ app.js
@@ -0,0 +1,5 @@
+alert("Welcome to our website! We're glad you're here.")
+alert("Error! Please enter a valid password")
+alert("Welcome to JS Land \nHappy Coding!")
+alert("Welcome to JS Land")
+alert("Happy Coding!")
