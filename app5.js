bb3e057533231d7a6f5182e533415f4a07b3c282
Ahtasham Mustafa <ahtshammustufa786@gmail.com>
2023-08-15 21:55:41 +0500

    Assignment 5 ongoing

---
+++ app5.js
@@ -0,0 +1,155 @@
+// Q1
+var a=3
+var b=5
+var sum=a+b
+document.write("The Sum of "+a+" and "+b+" "+"is "+sum)
+// Q2
+var subtract=a-b
+document.write("<br>")
+document.write("The Sub of "+a+" and "+b+" "+"is "+subtract)
+var multiple=a*b
+document.write("<br>")
+document.write("The multiple of "+a+" and "+b+" "+"is "+multiple)
+var division=a/b
+document.write("<br>")
+document.write("The division of "+a+" and "+b+" "+"is "+division)
+var mode=a%b
+document.write("<br>")
+document.write("The reminder of "+a+" and "+b+" "+"is "+mode)
+// Q3
+var value;
+document.write("<br>")
+document.write("<br>")
+document.write("Value after declaration is "+value)
+value=5
+document.write("<br>")
+document.write("Initial Value: "+value)
+++value
+document.write("<br>")
+document.write("Value after increment: "+value)
+value +=7
+document.write("<br>")
+document.write("Value after addition: "+value)
+--value 
+document.write("<br>")
+document.write("Value after decrement: "+value)
+value %=3 
+document.write("<br>")
+document.write("The reminder is: "+value)
+// Q4
+var ticketPrice=600
+var ticketBuyers=5
+var cost=ticketPrice*ticketBuyers
+document.write("<br>")
+document.write("<br>")
+document.write("The cost of to buy "+ticketBuyers+" tickets to a movie is "+cost+"PKR")
+// Q5
+var c=4
+var d=1
+var ans1=c*d
+document.write("<br>")
+document.write("<br>")
+document.write(c+" X "+d+" = "+ans1)
+var c=4
+var d=2
+var ans2=c*d
+document.write("<br>")
+document.write(c+" X "+d+" = "+ans2)
+var c=4
+var d=3
+var ans3=c*d
+document.write("<br>")
+document.write(c+" X "+d+" = "+ans3)
+var c=4
+var d=4
+var ans4=c*d
+document.write("<br>")
+document.write(c+" X "+d+" = "+ans4)
+var c=4
+var d=5
+var ans5=c*d
+document.write("<br>")
+document.write(c+" X "+d+" = "+ans5)
+var c=4
+var d=6
+var ans6=c*d
+document.write("<br>")
+document.write(c+" X "+d+" = "+ans6)
+var c=4
+var d=7
+var ans7=c*d
+document.write("<br>")
+document.write(c+" X "+d+" = "+ans7)
+var c=4
+var d=8
+var ans8=c*d
+document.write("<br>")
+document.write(c+" X "+d+" = "+ans8)
+var c=4
+var d=9
+var ans9=c*d
+document.write("<br>")
+document.write(c+" X "+d+" = "+ans9)
+var c=4
+var d=10
+var ans10=c*d
+document.write("<br>")
+document.write(c+" X "+d+" = "+ans10)
+// Q6
+var temp1=25
+var temp2=70
+var farenheit1=(temp1*9/5)+32
+document.write("<br>")
+document.write("<br>")
+document.write(temp1+"\u00B0C is "+farenheit1+"\u00B0F")
+var temp3=20
+var temp4=70
+var centigrade1=(temp4-32)*5/9
+document.write("<br>")
+document.write(temp4+"\u00B0F is "+centigrade1+"\u00B0C")
+// Q7
+var item1=650
+var item2=100
+var orderQty1=3
+var orderQty2=7
+var shippingCharges=100
+var totalCost=(item1*orderQty1)+(item2*orderQty2)+shippingCharges
+document.write("<br>")
+document.write("<br>")
+document.write("Price of Item 1 is "+item1)
+document.write("<br>")
+document.write("Quantity of Item 1 is"+orderQty1)
+document.write("<br>")
+document.write("Price of Item 2 is "+item2)
+document.write("<br>")
+document.write("Quantity of Item 2 is"+orderQty2)
+document.write("<br>")
+document.write("Shipping Charges "+shippingCharges)
+document.write("<br>")
+document.write("<br>")
+document.write("Total cost of your order is "+totalCost)
+// Q8
+var totalMarks=980
+var marksObtained=804
+var percentage=(marksObtained/totalMarks)*100
+document.write("<br>")
+document.write("<br>")
+document.write("Total Marks "+totalMarks)
+document.write("<br>")
+document.write("Marks Obtained "+marksObtained)
+document.write("<br>")
+document.write("Percentage "+percentage+"%")
+// Q9
+var usDollars=10
+var saudiRial=25
+var dollarRate=104.80
+var riyalRate=28
+var totalCurrency=(usDollars*dollarRate)+(saudiRial*riyalRate)
+document.write("<br>")
+document.write("<br>")
+document.write("Currency in PKR")
+document.write("<br>")
+document.write("Total Currency in PKR: "+totalCurrency)
+
+
+
