1b8e2f852f32d18476994d6e449ddf3da10de9b4
Ahtasham Mustafa <ahtshammustufa786@gmail.com>
2023-09-10 17:41:35 +0500

    Chapter 13 Assignment Done

---
+++ app12-13.js
@@ -0,0 +1,79 @@
+// // Q1
+// var value= prompt("Enter any Character (number or string)")
+// value = value.charCodeAt(0)
+// if (value >=65 && value <=90) {
+//     alert("Input is uppercase character")
+// } else if ((value >=97 && value <=122)) {
+//     alert("Input is lowercase character")
+// }else{
+//     alert("Input is a number")
+// }
+
+// Q2
+// var int1=+prompt("Enter the 1st Integer")
+// var int2=+prompt("Enter the 2nd Integer")
+// if (int1 > int2){
+//     alert("Integer 1 is greater")
+// }else if (int1 < int2){
+//     alert("Integer 2 is greater")
+// }else if(int1 == int2){
+//     alert("Both are equal")
+// }else {
+//     alert("Not a number")
+// }
+
+// Q3
+// var num=+prompt("Enter the number")
+// if (num < 0){
+//     alert("The number is negative")
+// }else if (num === 0){
+//     alert("The number is Zero")
+// }else if (num > 0){
+//     alert("The number is Positive")
+// }else {
+//     alert("Not a Number")
+// }
+
+// Q4
+// let character= prompt("Please write a small alpha to check it is vowel or not")
+// let char = character.toLowerCase()
+// if(char == 'a'||char == "e"||char == "i"||char == "o"||char == "u"){
+//     alert("Character is a vowel")
+// }else {
+//     alert("It is not a vowel")
+// }
+
+// Q5
+// let password= 123
+// let userpassword=prompt("Please enter your password")
+// if (userpassword == ""){
+//     alert("Please enter the password")
+// }else if(userpassword == password){
+//     alert("“Correct! The password you entered matches the original password")
+// }else{
+//     alert("Incorrect password")
+// }
+
+// Q6
+// var greeting;
+// var hour = 13;
+// if (hour < 18) {
+// greeting = "Good day"
+// alert(greeting)
+// }else{
+// greeting = "Good evening"
+// alert(greeting)
+// }
+
+// Q7
+// let time = +prompt("Enter time in 24 hours clock format like: 1900 = 7pm");
+// if(time >= 0001 && time < 1200){
+//     alert("Good Morning")
+// } else if(time >= 1200 && time < 1700) {
+//     alert("Good Afternoon")
+// }else if(time >= 1700 && time < 2100) {
+//     alert("Good Evening")
+// } else if(time >= 2100 && time <= 2359){ 
+//     alert("Good Night")
+// }
+
