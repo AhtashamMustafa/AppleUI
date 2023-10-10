51e791ba855acebac3b1ad8ac8894a7fc1695e3f
Ahtasham Mustafa <ahtshammustufa786@gmail.com>
2023-10-11 00:45:10 +0500

    Chapter 30 in Progress

---
+++ app26-30.js
@@ -0,0 +1,46 @@
+// Q1
+// var userInput=parseFloat(prompt("Enter a decimal integer"))
+// var roundOff=Math.round(userInput)
+// var floor=Math.floor(userInput)
+// var ceil=Math.ceil(userInput)
+// document.write(`Number: ${userInput}`)
+// document.write(`<br>`)  
+// document.write(`Round Off: ${roundOff}`)
+// document.write(`<br>`)  
+// document.write(`Floor: ${floor}`)
+// document.write(`<br>`)  
+// document.write(`Ceil: ${ceil}`)
+
+// Q2
+// var userInput=parseFloat(prompt("Enter a negative decimal integer"))
+// var roundOff=Math.round(userInput)
+// var floor=Math.floor(userInput)
+// var ceil=Math.ceil(userInput)
+// document.write(`Number: ${userInput}`)
+// document.write(`<br>`)  
+// document.write(`Round Off: ${roundOff}`)
+// document.write(`<br>`)  
+// document.write(`Floor: ${floor}`)
+// document.write(`<br>`)  
+// document.write(`Ceil: ${ceil}`)
+
+// Q3
+// var value= -35
+// var positiveValue=Math.abs(value)
+// document.write(`The Absolute value of ${value} is ${positiveValue}`)
+
+// Q4
+//  var dice =Math.round(Math.random()*5+1)
+//  document.write(`Random dice value is: ${dice}`)
+
+// Q5
+// var coin = Math.round(Math.random() * 1 + 1)
+// document.write(coin)
+// document.write("<br>")
+// if(coin==1){
+//     document.write(`Random Coin value is: Heads`)
+// }else{
+//     document.write(`Random Coin value is: Tails`)
+// }
+
+// Q6
